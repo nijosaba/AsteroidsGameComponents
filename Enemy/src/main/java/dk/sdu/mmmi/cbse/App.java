d8cef187c0f70588c44aaa3c749597629be4d772
NickBang <niban20@student.sdu.dk>
2025-02-05 13:25:36 +0100

    enemy.jar

---
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/App.java
@@ -0,0 +1,13 @@
+package dk.sdu.mmmi.cbse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
